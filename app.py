76cc87de5a9df350e1bf19f7eea1e90b7bbead91
whengl@naver.com <whengl@naver.com>
2026-08-19 14:48:24 +0900

    dashboard 수정2

--- app.py
+++ app.py
@@ -10,48 +10,40 @@ app = Flask(__name__)
 
 engine = create_engine("mysql+pymysql://project_user:project!%40#$@192.168.1.37/robot?charset=utf8mb4")
 
-
 @app.route("/dashboard")
 def dashboard():
-    with Session(engine) as db_session :
+    with Session(engine) as db_session:
 
         devices = db_session.scalars(
-                    select(Device).limit(4)
-                ).all()
+            select(Device).limit(4)
+        ).all()
+
         
+
         charts = []
 
         for device in devices:
-            stmt = (
-                select(
-                    func.hour(DeviceLog.recorded_at).label("hour"),
-                    func.round(func.avg(DeviceLog.value)).label("value")
-                )
-                .where(
-                    DeviceLog.device_seq == device.device_seq,
-                    func.date(DeviceLog.recorded_at) == func.curdate()
-                )
-                .group_by(
-                    func.hour(DeviceLog.recorded_at)
-                )
-                .order_by(
-                    func.hour(DeviceLog.recorded_at)
-                )
-            )
-            rows = db_session.execute(stmt).all()
+            value_codes = db_session.scalars(
+                select(DeviceLog.value_code)
+                .where(DeviceLog.device_seq == device.device_seq)
+                .distinct()
+            ).all()
+            
+            for value_code in value_codes:
+                chart = get_sensor_chart(device.device_seq,value_code)
+                
+                charts.append({
+                    "device_seq": device.device_seq,
+                    "device_name": device.device_name,
+                    "value_code": value_code,
+                    "labels": chart["labels"],
+                    "values": chart["values"]
+                })
 
-            charts.append({
-                "device_seq": device.device_seq,
-                "device_name": device.device_name,
-                "labels":[f"{row.hour}:00" for row in rows],
-                "values": [float(row.value) if row.value is not None else 0.0 for row in rows]
-            })
+        return render_template("dashboard.html",charts=charts)
 
 
-        return render_template("dashboard.html", charts = charts)
-
-
-def get_sensor_chart(device_seq,selected_date = None):
+def get_sensor_chart(device_seq,value_name,selected_date = None):
     with Session(engine) as db_session:
 
         if selected_date is None:
@@ -59,34 +51,43 @@ def get_sensor_chart(device_seq,selected_date = None):
         else:
             target_date = selected_date
 
+        
+
         stmt = (
             select(
                 func.hour(DeviceLog.recorded_at).label("hour"),
-                func.round(func.avg(DeviceLog.value)).label("value")
+                func.minute(DeviceLog.recorded_at).label("minute"),
+                func.round(func.avg(DeviceLog.value), 1).label("value")
             )
             .where(
                 DeviceLog.device_seq == device_seq,
+                DeviceLog.value_code == value_name,
                 func.date(DeviceLog.recorded_at) == target_date
             )
             .group_by(
-                func.hour(DeviceLog.recorded_at)
+                func.hour(DeviceLog.recorded_at),
+                func.minute(DeviceLog.recorded_at)
             )
             .order_by(
-                func.hour(DeviceLog.recorded_at)
+                func.hour(DeviceLog.recorded_at),
+                func.minute(DeviceLog.recorded_at)
             )
         )
 
         
         rows = db_session.execute(stmt).all()
         
-        print(rows)
+        for row in rows:
+            print(row.hour, row.minute, row.value)
 
-        return { "labels": [f"{row.hour}:00"for row in rows],
-                 "values": [float(row.value)for row in rows] }
+        return { "labels": [f"{row.hour:02d}:{row.minute:02d}"for row in rows],
+                 "values": [float(row.value)for row in rows]
+                }
 
 @app.get("/temp")
 def temp():
-    chart = get_sensor_chart(1)
+    chart = get_sensor_chart(1,'temp')
+    
 
     return render_template("temp.html",labels=chart["labels"],value=chart["values"])
 
@@ -95,13 +96,13 @@ def temp_chart_date():
 
     selected_date = request.get_json()['date'];
 
-    chart = get_sensor_chart(1,selected_date)
+    chart = get_sensor_chart(1,'temp',selected_date)
 
     return jsonify(labels=chart["labels"],value=chart["values"])
 
 @app.get("/hum")
 def hum():
-    chart = get_sensor_chart(2)
+    chart = get_sensor_chart(1,'hum')
 
     return render_template("hum.html",labels=chart["labels"],value=chart["values"])
 
@@ -110,13 +111,13 @@ def hum_chart_date():
 
     selected_date = request.get_json()['date'];
 
-    chart = get_sensor_chart(2,selected_date)
+    chart = get_sensor_chart(1,'hum',selected_date)
 
     return jsonify(labels=chart["labels"],value=chart["values"])
 
 @app.get("/dust")
 def dust():
-    chart = get_sensor_chart(3)
+    chart = get_sensor_chart(2,'dust')
     
     return render_template("dust.html",labels=chart["labels"],value=chart["values"])
 
@@ -125,13 +126,13 @@ def dust_chart_date():
 
     selected_date = request.get_json()['date'];
 
-    chart = get_sensor_chart(3,selected_date)
+    chart = get_sensor_chart(2,'dust',selected_date)
 
     return jsonify(labels=chart["labels"],value=chart["values"])
 
 @app.get("/co2")
 def co2():
-    chart = get_sensor_chart(4)
+    chart = get_sensor_chart(3,'co2')
         
     return render_template("co2.html",labels=chart["labels"],value=chart["values"])
 
@@ -140,7 +141,23 @@ def co2_chart_date():
 
     selected_date = request.get_json()['date'];
 
-    chart = get_sensor_chart(4,selected_date)
+    chart = get_sensor_chart(3,'co2',selected_date)
+
+    return jsonify(labels=chart["labels"],value=chart["values"])
+
+
+@app.get("/pm25")
+def pm25():
+    chart = get_sensor_chart(3,'pm25')
+        
+    return render_template("pm25.html",labels=chart["labels"],value=chart["values"])
+
+@app.post("/api/pm25")
+def pm25_chart_date():
+
+    selected_date = request.get_json()['date'];
+
+    chart = get_sensor_chart(3,'pm25',selected_date)
 
     return jsonify(labels=chart["labels"],value=chart["values"])
 
